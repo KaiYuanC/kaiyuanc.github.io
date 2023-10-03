f079f59ecfda55cd0276bffb0eb3f6f6d3474590
KaiYuanC <chikaiyuan@gmail.com>
2023-10-03 01:16:55 -0400

    Add static exports

---
+++ dist/_next/static/t3GXb-vWqXFfVjSyaoe0d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-c57439bfef35e6a6.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
