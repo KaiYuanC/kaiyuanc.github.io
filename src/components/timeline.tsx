3cf2694be3c704e65ef5cd0f5c08b72bd39b2926
KaiYuanC <chikaiyuan@gmail.com>
2023-10-07 23:57:02 -0400

    Add timeline skeleton

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,17 +1,24 @@
+import Link from "next/link";
 import React from "react";
 import { items } from "./timeline-items.json";
 import styles from "./timeline.module.css";
 
 /*
-  TODO: inforce better types below
+  TODO: 
+  - inforce better types below
+  - figure out how to highlight/underline in json files
+  - firgure out how to add links efficiently
+
 */
 
 type TimelineItem = {
   date: string;
-  location: string;
+  location: string | undefined;
   type: string;
   title: string;
   description: string | undefined;
+  link: string | undefined;
+  linkText: string | undefined;
 };
 
 const Items = () => {
@@ -20,9 +27,15 @@ const Items = () => {
       {items.map((item: TimelineItem) => {
         return (
           <div className={styles.item}>
-            <h1>{item.date}, {item.location}</h1>
-            <h2>{item.title}</h2>
-            {item.description && <h2>{item.description}</h2>}
+            <h3 className={styles.date}>{item.date}</h3>
+            <div className={styles.itemInfo}>
+              <h1>{item.title}</h1>
+              <h2>{item.description}</h2>
+              {item.link && <Link href={item.link}>{item.linkText}</Link>}
+              <h4>
+                {item.location} {item.location && "|"} {item.type}
+              </h4>
+            </div>
           </div>
         );
       })}
@@ -32,11 +45,22 @@ const Items = () => {
 
 const Timeline = () => {
   return (
-  <div className={styles.timeline}>
-    <hr className="my-8 h-px border-t-0 bg-black" />
-    <h1>What I am up to...</h1>
-    <Items/>
-  </div>);
+    <div className={styles.timeline}>
+      <hr className="my-8 h-px border-t-0 bg-black" />
+      <div className={styles.title}>Moments</div>
+      <div className={styles.content}>
+        <Items />
+        <div className={styles.filter}>
+          <h2>Filter</h2>
+          <Link href="add later">Work & Internship</Link>
+          <Link href="add later">Projects</Link>
+          <Link href="add later">Education</Link>
+          <Link href="add later">Life</Link>
+          <Link href="add later">Writing</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
